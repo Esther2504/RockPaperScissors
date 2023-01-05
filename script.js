a20ff1bdd7967059c5cfb27a51c843b16847d540
Esther2504 <esther252000@gmail.com>
2023-01-05 15:08:41 +0100

    finished game

--- script.js
+++ script.js
@@ -1,69 +1,42 @@
-
-
 function startGame(param) {
-const a = param
-const choices = ["rock", "paper", "scissors"];
-const random = Math.floor(Math.random() * choices.length);
-const b = choices[random]
+  const a = param;
+  const choices = ["rock", "paper", "scissors"];
+  const random = Math.floor(Math.random() * choices.length);
+  const b = choices[random];
 
-const playerchoice = document.getElementById('playerchoice')
-const computerchoice = document.getElementById('computerchoice')
-const result = document.getElementById('result')
+  const playerchoice = document.getElementById("playerchoice");
+  const computerchoice = document.getElementById("computerchoice");
+  const result = document.getElementById("result");
 
-if (a == 'rock') {
-    playerchoice.src = "rock.svg"
-} else if (a == 'paper') {
-    playerchoice.src = "paper.svg"
-} else if (a == 'scissors') {
-    playerchoice.src = "scissors.svg"
+  if (a == "rock") {
+    playerchoice.src = "rock.svg";
+  } else if (a == "paper") {
+    playerchoice.src = "paper.svg";
+  } else if (a == "scissors") {
+    playerchoice.src = "scissors.svg";
+  }
+
+  if (b == "rock") {
+    computerchoice.src = "rockcomp.svg";
+  } else if (b == "paper") {
+    computerchoice.src = "papercomp.svg";
+  } else if (b == "scissors") {
+    computerchoice.src = "scissorscomp.svg";
+  }
+
+  if (a == "rock" && b == "scissors") {
+    result.innerText = "You won!";
+  } else if (a == "rock" && b == "paper") {
+    result.innerText = "Computer won!";
+  } else if (a == "scissors" && b == "paper") {
+    result.innerText = "You won!";
+  } else if (a == "scissors" && b == "rock") {
+    result.innerText = "Computer won!";
+  } else if (a == "paper" && b == "rock") {
+    result.innerText = "You won!";
+  } else if (a == "paper" && b == "scissors") {
+    result.innerText = "Computer won!";
+  } else if (a == b) {
+    result.innerText = `It's a draw!`;
+  }
 }
-
-if (b == 'rock') {
-    computerchoice.src = "rockcomp.svg"
-} else if (b == 'paper') {
-    computerchoice.src = "papercomp.svg"
-} else if (b == 'scissors') {
-    computerchoice.src = "scissorscomp.svg"
-}
-
-
-
-let pointsplayer;
-let pointscomputer;
-
-console.log('You chose ' + a)
-console.log('Computer chose ' + b)
-
-if ((a == 'rock') && (b == 'scissors')) {
-    alert('You won!')
-    result.innerText = 'You won!'
-    pointsplayer++
-} else if ((a == 'rock') && (b == 'paper')) {
-    alert('Computer won!')
-    result.innerText = 'Computer won!'
-    pointscomputer++
-} else if ((a == 'scissors') && (b == 'paper')) {
-    alert('You won!')
-    pointsplayer++
-} else if ((a == 'scissors') && (b == 'rock')) {
-    console.log('Computer won!')
-    pointscomputer++
-}else if ((a == 'paper') && (b == 'rock')) {
-    alert('You won!')
-    pointsplayer++
-}else if ((a == 'paper') && (b == 'scissors')) {
-    alert('Computer won!')
-    pointscomputer++
-} else if (a == b) {
-    alert(`It's a draw!`)
-    pointsplayer++
-    pointscomputer++
-}
-
-console.log(pointscomputer)
-console.log(pointsplayer)
-
-}
-
-
-
