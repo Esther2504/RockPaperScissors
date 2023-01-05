55ee7569643c76580e717d0d6c61070f2b100b77
Esther2504 <esther252000@gmail.com>
2023-01-05 14:36:34 +0100

    styling

--- script.js
+++ script.js
@@ -2,37 +2,66 @@
 
 function startGame(param) {
 const a = param
-
-// let random = Math.random() * 3;
 const choices = ["rock", "paper", "scissors"];
-
 const random = Math.floor(Math.random() * choices.length);
 const b = choices[random]
 
-console.log('player chose ' + a)
-console.log('computer chose ' + b)
+const playerchoice = document.getElementById('playerchoice')
+const computerchoice = document.getElementById('computerchoice')
+const result = document.getElementById('result')
+
+if (a == 'rock') {
+    playerchoice.src = "rock.svg"
+} else if (a == 'paper') {
+    playerchoice.src = "paper.svg"
+} else if (a == 'scissors') {
+    playerchoice.src = "scissors.svg"
+}
+
+if (b == 'rock') {
+    computerchoice.src = "rockcomp.svg"
+} else if (b == 'paper') {
+    computerchoice.src = "papercomp.svg"
+} else if (b == 'scissors') {
+    computerchoice.src = "scissorscomp.svg"
+}
+
+
+
+let pointsplayer;
+let pointscomputer;
+
+console.log('You chose ' + a)
+console.log('Computer chose ' + b)
 
 if ((a == 'rock') && (b == 'scissors')) {
-    console.log(b)
-    console.log('player won!')
+    alert('You won!')
+    result.innerText = 'You won!'
+    pointsplayer++
 } else if ((a == 'rock') && (b == 'paper')) {
-    console.log(b)
-    console.log('computer won!')
+    alert('Computer won!')
+    result.innerText = 'Computer won!'
+    pointscomputer++
 } else if ((a == 'scissors') && (b == 'paper')) {
-    console.log(b)
-    console.log('player2 won')
+    alert('You won!')
+    pointsplayer++
 } else if ((a == 'scissors') && (b == 'rock')) {
-    console.log(b)
-    console.log('computer3 won!')
+    console.log('Computer won!')
+    pointscomputer++
 }else if ((a == 'paper') && (b == 'rock')) {
-    console.log(b)
-    console.log('player4 won!')
+    alert('You won!')
+    pointsplayer++
 }else if ((a == 'paper') && (b == 'scissors')) {
-    console.log(b)
-    console.log('computer5 won!')
+    alert('Computer won!')
+    pointscomputer++
 } else if (a == b) {
-    console.log('draw')
+    alert(`It's a draw!`)
+    pointsplayer++
+    pointscomputer++
 }
+
+console.log(pointscomputer)
+console.log(pointsplayer)
 
 }
 
